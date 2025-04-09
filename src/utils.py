6f8271e2d6a03b48a9f80143f1fd915577a06e4e
vadimko4 <vad.informat@gmail.com>
2025-04-09 09:29:17 +0400

    add structure src and tests directories

--- src/utils.py
+++ src/utils.py
@@ -1,2 +1,27 @@
 def user_interaction():
     pass
+
+
+def filter_vacancies(vacancies, filter_words):
+    """фильтрует список вакансий по поисковым словам"""
+    pass
+
+
+def get_vacancies_by_salary(vacancies, salary_range):
+    """фильтрует вакансии по зарплатам"""
+    pass
+
+
+def sort_vacancies(vacancies):
+    """сортирует вакансии"""
+    pass
+
+
+def get_top_vacancies(vacancies, top_n):
+    """возвращает топ n вакансий"""
+    pass
+
+
+def print_vacancies(vacancies):
+    """выводит список вакансий в удобном для чтения виде"""
+    pass
