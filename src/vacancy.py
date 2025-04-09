7c04cd9ed9b05100d50ba2079d68c3200af1592f
vadimko4 <vad.informat@gmail.com>
2025-04-09 10:27:14 +0400

    add vacancy str

--- src/vacancy.py
+++ src/vacancy.py
@@ -7,13 +7,17 @@ class Vacancy:
     название вакансии, ссылка на вакансию, зарплата, краткое описание или требования
     """
 
-    def __init__(self, vacancy_name, vacancy_link, salary_from, salary_to, vacancy_desription):
+    def __init__(self, vacancy_title, vacancy_link, salary_from, salary_to, vacancy_desription):
         """конструктор вакансии"""
-        self.vacancy_name = vacancy_name
+        self.vacancy_title = vacancy_title
         self.vacancy_link = vacancy_link
         self.salary_from = salary_from
         self.salary_to = salary_to
         self.vacancy_desription = vacancy_desription
+
+    def __str__(self):
+        return (f"{self.vacancy_title}: зарплата от {self.salary_from}, до {self.salary_to}; "
+                f"{self.vacancy_desription}; {self.vacancy_link}")
 
     def __lt__(self, other):
         """
