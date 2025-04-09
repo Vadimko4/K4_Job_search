fd0116ba0e59ab47ba545eb87c39d2009b98819e
vadimko4 <vad.informat@gmail.com>
2025-04-09 10:16:51 +0400

    add vacancy lt

--- src/utils.py
+++ src/utils.py
@@ -13,8 +13,8 @@ def get_vacancies_by_salary(vacancies, salary_range):
 
 
 def sort_vacancies(vacancies):
-    """сортирует вакансии"""
-    pass
+    """сортирует вакансии по убыванию актуальной зарплаты"""
+    return sorted(vacancies, reverse=True)
 
 
 def get_top_vacancies(vacancies, top_n):
--- src/vacancy.py
+++ src/vacancy.py
@@ -15,3 +15,33 @@ class Vacancy:
         self.salary_to = salary_to
         self.vacancy_desription = vacancy_desription
 
+    def __lt__(self, other):
+        """
+        метод определяет меньше self, чем other или нет
+        сравнение производится по актуальной зарплате:
+        если есть и нижний и верхний пределы, то это их среднее арифметическое
+        если есть только одно из значений, то это оно
+        """
+        if self.salary_to and self.salary_from:  # определяем актуальную зарплату для self
+            self_salary = (self.salary_to + self.salary_from) / 2
+        elif self.salary_to:
+            self_salary = self.salary_to
+        else:
+            self_salary = self.salary_from
+        
+        if other.salary_to and other.salary_from:  # определяем актуальную зарплату для other
+            other_salary = (other.salary_to + other.salary_from) / 2
+        elif other.salary_to:
+            other_salary = other.salary_to
+        else:
+            other_salary = other.salary_from
+
+        return self_salary < other_salary
+
+    def __gt__(self, other):
+        """метод определяет больше self, чем other или нет"""
+        pass
+
+    def __eq__(self, other):
+        """метод определяет равны ли self и other между собой"""
+        pass
